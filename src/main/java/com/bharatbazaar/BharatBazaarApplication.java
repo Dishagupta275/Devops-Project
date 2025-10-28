c1eb03360ebad2ed4ff55ca7d80edaef6903d9c8
Dishagupta275 <Dishag2705@gmail.com>
2025-10-28 21:55:13 +0530

    change

--- src/main/java/com/bharatbazaar/BharatBazaarApplication.java
+++ src/main/java/com/bharatbazaar/BharatBazaarApplication.java
@@ -3,7 +3,7 @@ package com.bharatbazaar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 public class BharatBazaarApplication {
     public static void main(String[] args) {
         SpringApplication.run(BharatBazaarApplication.class, args);
